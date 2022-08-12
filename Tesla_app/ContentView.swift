b25f8d7a8462c9d5825e7ca3e80406395fecb9d1
Abdulloh <abdullohbahromjonv@gmail.com>
2022-08-12 19:00:31 +0500

    Initial Commit

---
+++ Tesla_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tesla_app
+//
+//  Created by Abdulloh on 12/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tesla_app/Tesla_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tesla_appApp.swift
+//  Tesla_app
+//
+//  Created by Abdulloh on 12/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Tesla_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
